9e42a493e380d50e43203c3915df24a8067f24dc
Stefanie <35500567+histef@users.noreply.github.com>
2018-11-13 20:20:43 -0800

    adding all files

---
+++ precache-manifest.bd0d9fce37c26c092dd416c22564cc5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6d7a91fc58707f33e9806e1536c41f0",
+    "url": "/static/media/matt-collamer-555626-unsplash.f6d7a91f.jpg"
+  },
+  {
+    "revision": "ccc2aab683d69c190d5f688ba9e2b153",
+    "url": "/static/media/LA-landscape.ccc2aab6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc916f51a68c73244cad",
+    "url": "/static/js/main.dc916f51.chunk.js"
+  },
+  {
+    "revision": "24531f1447a2515b689f",
+    "url": "/static/js/1.24531f14.chunk.js"
+  },
+  {
+    "revision": "dc916f51a68c73244cad",
+    "url": "/static/css/main.1d56ec61.chunk.css"
+  },
+  {
+    "revision": "e63b0fbc35a6a98cc3d9be21fa89f01e",
+    "url": "/index.html"
+  }
+];
